f652c9fc4fde8f04fa9a147436b491dae32564f0
PerryMartin <perry.martin@ocsbstudent.ca>
2015-11-02 13:22:56 -0500

    v1.0

---
+++ houseLocation/location.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace houseLocation
+{
+    partial class frmlocationOfHouse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAptNumber = new System.Windows.Forms.Label();
+            this.lblStreetAddress = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.lblPostalCode = new System.Windows.Forms.Label();
+            this.txtAptNumber = new System.Windows.Forms.TextBox();
+            this.txtStreetAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtProvince = new System.Windows.Forms.TextBox();
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAptNumber
+            // 
+            this.lblAptNumber.AutoSize = true;
+            this.lblAptNumber.Location = new System.Drawing.Point(38, 41);
+            this.lblAptNumber.Name = "lblAptNumber";
+            this.lblAptNumber.Size = new System.Drawing.Size(66, 13);
+            this.lblAptNumber.TabIndex = 0;
+            this.lblAptNumber.Text = "Apt. Number";
+            // 
+            // lblStreetAddress
+            // 
+            this.lblStreetAddress.AutoSize = true;
+            this.lblStreetAddress.Location = new System.Drawing.Point(38, 68);
+            this.lblStreetAddress.Name = "lblStreetAddress";
+            this.lblStreetAddress.Size = new System.Drawing.Size(76, 13);
+            this.lblStreetAddress.TabIndex = 1;
+            this.lblStreetAddress.Text = "Street Address";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(38, 93);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(24, 13);
+            this.lblCity.TabIndex = 2;
+            this.lblCity.Text = "City";
+            // 
+            // lblProvince
+            // 
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.Location = new System.Drawing.Point(38, 121);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(49, 13);
+            this.lblProvince.TabIndex = 3;
+            this.lblProvince.Text = "Province";
+            // 
+            // lblPostalCode
+            // 
+            this.lblPostalCode.AutoSize = true;
+            this.lblPostalCode.Location = new System.Drawing.Point(38, 149);
+            this.lblPostalCode.Name = "lblPostalCode";
+            this.lblPostalCode.Size = new System.Drawing.Size(64, 13);
+            this.lblPostalCode.TabIndex = 4;
+            this.lblPostalCode.Text = "Postal Code";
+            // 
+            // txtAptNumber
+            // 
+            this.txtAptNumber.Location = new System.Drawing.Point(133, 33);
+            this.txtAptNumber.Name = "txtAptNumber";
+            this.txtAptNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtAptNumber.TabIndex = 5;
+            // 
+            // txtStreetAddress
+            // 
+            this.txtStreetAddress.Location = new System.Drawing.Point(133, 60);
+            this.txtStreetAddress.Name = "txtStreetAddress";
+            this.txtStreetAddress.Size = new System.Drawing.Size(100, 20);
+            this.txtStreetAddress.TabIndex = 6;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(133, 86);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 7;
+            // 
+            // txtProvince
+            // 
+            this.txtProvince.Location = new System.Drawing.Point(133, 114);
+            this.txtProvince.Name = "txtProvince";
+            this.txtProvince.Size = new System.Drawing.Size(100, 20);
+            this.txtProvince.TabIndex = 8;
+            // 
+            // txtPostalCode
+            // 
+            this.txtPostalCode.Location = new System.Drawing.Point(133, 141);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(100, 20);
+            this.txtPostalCode.TabIndex = 9;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(12, 225);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(301, 23);
+            this.btnShow.TabIndex = 10;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // frmlocationOfHouse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 278);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.txtPostalCode);
+            this.Controls.Add(this.txtProvince);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtStreetAddress);
+            this.Controls.Add(this.txtAptNumber);
+            this.Controls.Add(this.lblPostalCode);
+            this.Controls.Add(this.lblProvince);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblStreetAddress);
+            this.Controls.Add(this.lblAptNumber);
+            this.Name = "frmlocationOfHouse";
+            this.Text = "Loction";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAptNumber;
+        private System.Windows.Forms.Label lblStreetAddress;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblProvince;
+        private System.Windows.Forms.Label lblPostalCode;
+        private System.Windows.Forms.TextBox txtAptNumber;
+        private System.Windows.Forms.TextBox txtStreetAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtProvince;
+        private System.Windows.Forms.TextBox txtPostalCode;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ houseLocation/location.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Created by: Perry Martin
+ * Created on: 02-11-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit#4-03
+ * This program displays your address
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace houseLocation
+{
+    public partial class frmlocationOfHouse : Form
+    {
+        private void ShowAddress(string aptNumber, string streetAddress, string city, string province, string postalCode)
+        {
+            MessageBox.Show("Apartment " + aptNumber + ", " + streetAddress + ", " + city + ", " + province + ", " + postalCode);
+        }
+
+        private void ShowAddress(string streetAddress, string city, string province, string postalCode)
+        {
+            MessageBox.Show(streetAddress + ", " + city + ", " + province + ", " + postalCode);
+        }
+
+        public frmlocationOfHouse()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            string aptNumberInput = this.txtAptNumber.Text;
+            string streetAddressInput = this.txtStreetAddress.Text;
+            string cityInput = this.txtCity.Text;
+            string provinceInput = this.txtProvince.Text;
+            string postalCodeInput = this.txtPostalCode.Text;
+
+            if (aptNumberInput == "")
+            {
+                ShowAddress(streetAddressInput, cityInput, provinceInput, postalCodeInput);
+            }
+            else
+            {
+                ShowAddress(aptNumberInput, streetAddressInput, cityInput, provinceInput, postalCodeInput);
+            }
+
+        }
+    }
+}
